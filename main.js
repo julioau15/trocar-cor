cfc2c64743e3a636294547dfce35ad8a4b82c86c
julioau15 <julioaugustov@gmail.com>
2026-02-02 11:16:04 -0300

    adicionado funcionalidade das cores em português (Vermelho, Amarelo, Azul, Verde)

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 'use strict'
 const btnTrocar = document.getElementById('btn-trocar')
-
+const txtInput = document.getElementById('txt-input')
 function trocarCor(){
-    const txtInput = document.getElementById('txt-input')
-    
     if(txtInput.value == ''){
         alert('[ERRO] O campo está vazio')
     }else{
-        document.documentElement.style.setProperty('--cor-bg', txtInput.value)
+        verificarCor(txtInput.value.toLowerCase())
     }
 }
-
+function verificarCor(cor){
+    let traduzirCor = document.documentElement.style
+    if(cor == 'vermelho'){
+        traduzirCor.setProperty('--cor-bg', 'red')
+    }else if(cor == 'azul'){
+        traduzirCor.setProperty('--cor-bg', 'blue')
+    }else if(cor == 'amarelo'){
+        traduzirCor.setProperty('--cor-bg', 'yellow')
+    }else if(cor == 'verde'){
+        traduzirCor.setProperty('--cor-bg', 'green')
+    }else{
+        traduzirCor.setProperty('--cor-bg', cor)
+    }
+}
 btnTrocar.addEventListener('click', trocarCor)
